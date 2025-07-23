e2738582c3a12112e2acc87958b3286096dfed57
Brandon Stecklein <brandon@ape-apps.com>
2025-07-23 08:40:49 -0500

    inv-top

--- src/index.js
+++ src/index.js
@@ -8450,7 +8450,7 @@ async function doWorkCanvasChunk(instance, data, callback) {
             let waterNeighbor = false;
 
             const topTxX = (x * useTextureSize) / atlasWidth;
-            const topTxY = (z * useTextureSize) / atlasHeight;
+            const topTxY = 1 - ((z * useTextureSize) / atlasHeight);
 
             const txPerW = useTextureSize / atlasWidth;
             const txPerH = useTextureSize / atlasHeight;
